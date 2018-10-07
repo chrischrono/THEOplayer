93f6e62b5d85de8aad7e695a5839e3fdc4672bf8
Christian Huang <christian@christians-imac.local>
2018-10-07 19:43:35 +0700

    Initial Commit

---
+++ THEOplayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  THEOplayer
+//
+//  Created by Christian  Huang on 07/10/18.
+//  Copyright © 2018 Christian Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
